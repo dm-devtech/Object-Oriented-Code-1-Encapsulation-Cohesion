01c4e180ded552fccc479fa0b01f34c8a7d11cfe
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-09-30 13:16:10 +0100

    Refactoring calculator classes

--- Exercise3.rb
+++ Exercise3.rb
@@ -1,21 +1,45 @@
 ## Exercise 3 (Refactoring the above code into two classes)
 
 class Calculator_addition
-  def add(number_1, number_2) #Group 1 - addition
-    number_1 + number_2
+  attr_reader :calculation, :answer
+
+  def initialize
+    @calculation = calculation
   end
 
-  def print_answer(answer) #Group 1 printing answer
-    "The Answer is: #{ answer }"
+  def add(number_1, number_2) #Group 1 - addition
+    @calculation = number_1 + number_2
   end
+
+  def print_answer(answer = @calculation) #Group 1 printing answer
+    puts "The Answer is: #{ answer }"
+  end
+
 end
 
 class Calculator_subtraction
-  def subtract(number_1, number_2) #Group 2 - subtraction
-    number_1 - number_2
+  attr_reader :calculation, :answer
+
+  def initialize
+    @calculation = calculation
   end
 
-  def print_answer(answer) #Group 2 printing answer
-    "The Answer is: #{ answer }"
+  def subtract(number_1, number_2) #Group 2 - subtraction
+    @calculation = number_1 - number_2
+  end
+
+  def print_answer(answer = @calculation ) #Group 2 printing answer
+    puts "The Answer is: #{ answer }"
   end
  end
+
+addition = Calculator_addition.new
+minus = Calculator_subtraction.new
+
+puts "addition class: 1+2"
+addition.add(1, 2)
+addition.print_answer()
+
+puts "subtraction class 20-16"
+minus.subtract(20, 16)
+minus.print_answer()
